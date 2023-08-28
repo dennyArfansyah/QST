3cb75d935d257c5fc071f73ac045a0de6ad44da9
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-28 17:30:55 +0700

    Initial Commit

---
+++ QST/QSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  QSTApp.swift
+//  QST
+//
+//  Created by Denny Arfansyah on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct QSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
